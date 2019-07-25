dc56ba9ed8a4121d63b93d4bc910cf426e4ec5cb
Unarine Singo <una.singo@trintel.co.za>
2019-07-25 13:51:05 +0200

    .RData upload

--- src/dataPrep.R
+++ src/dataPrep.R
@@ -1,1 +1,13 @@
-# processes raw data and returns a suitable object that will be used by the recomender predict functions.
+# Takes raw data and produces an rData file that will be used for analysis
+# Una Singo
+# 25 July 2019
+library(tidyverse)
+
+movies <- read.csv("data/Raw/ml-20m/movies.csv") 
+ratings <-  read.csv("data/Raw/ml-20m/ratings.csv")
+ratings <- left_join(movies, ratings) #j
+# select the first 6000 ratings
+ratings <- subset(ratings, userId<=6000)
+rm(movies)
+
+save.image('data/Raw/ratings.RData')
