f324b4feb3fdb926f0b7e163a47009b46d4d1345
Kaluba Chikonde <kaluba.c96@gmail.com>
2019-08-09 12:11:10 +0200

    item based adjustments

--- src/itemBased.R
+++ src/itemBased.R
@@ -2,5 +2,33 @@
 # Kaluba
 
 library(tidyverse)
+library(dplyr)
 load('data/Processed/recommender.RData')
+load('data/Processed/recommender2.RData')
 set.seed(1)
+
+head(ratings)
+movieId <- ratings$movieId
+userId <- ratings$userId
+ratings <- ratings$rating
+
+matrix <- as_tibble(cbind(movieId,userId,ratings))
+
+matrix <- as_tibble(ratings)
+
+viewed <- matrix %>%  select(userId, movieId, rating) %>% spread(key = movieId, value = rating)
+viewed_movies <- t(viewed)
+head(viewed)
+
+
+
+# function calculating cosine similarity
+cosine_sim <- function(a, b){
+  crossprod(a, b) / sqrt(crossprod(a) * crossprod(b))
+}
+
+?complete
+head(ratings)
+head(viewedMoviesMatrix) 
+head(viewed_movies)
+head(ratings_red)
